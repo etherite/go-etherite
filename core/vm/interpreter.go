5f4dd87b2f6e6687a0afda4e9085206ba90b4ceb
datdylan <datdylanyo@gmail.com>
2017-10-15 15:28:07 -0400

    core: fix paths after go-ethereum DoS fix push

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -20,10 +20,10 @@ import (
 	"fmt"
 	"sync/atomic"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/params"
+	"github.com/etherite/go-etherite/common"
+	"github.com/etherite/go-etherite/common/math"
+	"github.com/etherite/go-etherite/crypto"
+	"github.com/etherite/go-etherite/params"
 )
 
 // Config are the configuration options for the Interpreter
